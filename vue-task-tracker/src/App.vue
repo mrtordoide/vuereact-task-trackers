ec4ffcaadb7569c6d60fccc08aa1a9e7b5f7edcb
Amit Shrestha <mrtordoide@192-168-1-103.tpgi.com.au>
2021-10-01 11:08:09 +1000

    vue+react files combined, initial commit

---
+++ vue-task-tracker/src/App.vue
@@ -0,0 +1,112 @@
+<template>
+  <div class="container">
+    <Header @toggle-add-task="toggleAddTask" title="Task Tracker" :showAddTask="showAddTask" />
+    <div v-show="showAddTask">
+      <AddTask @add-task = "addTask" />
+    </div>
+    <Tasks @toggle-reminder="toggleReminder" @delete-task="deleteTask" :tasks="tasks" />
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue'
+import Tasks from './components/Tasks.vue'
+import AddTask from './components/AddTask.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Tasks,
+    AddTask
+  },
+  data(){
+    return {
+      tasks: [],
+      showAddTask: false,
+    }
+  },
+  methods: {
+    deleteTask(id){
+      if(confirm('Are you sure?')){
+        this.tasks = this.tasks.filter((task) => task.id != id)
+      }
+    },
+    toggleReminder(id){
+      this.tasks = this.tasks.map((task) => task.id == id ? {...task, reminder: !task.reminder} : task)
+    },
+    addTask(task){
+      this.tasks = [...this.tasks, task]
+    },
+    toggleAddTask(){
+      this.showAddTask = !this.showAddTask
+    }
+  },
+  created(){
+    this.tasks = [
+      {
+          id: 1,
+          text: 'Doctors Appointment',
+          day: 'Feb 5th at 2:30pm',
+          reminder: true,
+      },
+      {
+          id: 2,
+          text: 'COVID Vaccine Appointment',
+          day: 'Feb 6th at 1:30pm',
+          reminder: true,
+      },
+      {
+          id: 3,
+          text: 'Groceries Shopping',
+          day: 'Mar 5th at 2:30pm',
+          reminder: false,
+      }
+    ]
+  }
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap');
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+body {
+  font-family: 'Poppins', sans-serif;
+}
+.container {
+  max-width: 500px;
+  margin: 30px auto;
+  overflow: auto;
+  min-height: 300px;
+  border: 1px solid steelblue;
+  padding: 30px;
+  border-radius: 5px;
+}
+.btn {
+  display: inline-block;
+  background: #000;
+  color: #fff;
+  border: none;
+  padding: 10px 20px;
+  margin: 5px;
+  border-radius: 5px;
+  cursor: pointer;
+  text-decoration: none;
+  font-size: 15px;
+  font-family: inherit;
+}
+.btn:focus {
+  outline: none;
+}
+.btn:active {
+  transform: scale(0.98);
+}
+.btn-block {
+  display: block;
+  width: 100%;
+}
+</style>
